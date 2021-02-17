18a7cffc7de0f288f73b42cd7d89f7c8af13cc5d
michele <miki-vale@hotmail.it>
2021-02-17 20:42:08 +0100

    Create Repo

---
+++ ttt/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ttt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var result = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions {
+                Title = "Please pick a photo"
+            });
+
+            var stream = await result.OpenReadAsync();
+
+            resultImage.Source = ImageSource.FromStream(() => stream);
+        }
+    }
+}
